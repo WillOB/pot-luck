b13b154c400767b61b9204f16dcaeece33811495
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-10-06 14:49:43 -0600

    work on stuff

--- rsvp.rb
+++ rsvp.rb
@@ -1,6 +1,5 @@
 require './scan'
-food = open("./foods.csv")
-
+food = File.read('foods.csv')
 
 print "Enter your name: "
 name = scan
@@ -8,16 +7,22 @@ print "Enter your email address: "
 email = scan
 print "Enter number of additional guests: "
 extras = scan
-puts "What food will you bring? Here is the list of required items: #{food}"
+puts "What food will you bring? Here is the list of required items:\n #{food}"
 print "> "
 bring = scan
+bring.downcase!
 
-while food.downcase.include?(bring) == false
+while !food.include?(bring)
   print "We don't need that. Enter a food from the list: "
-  food = scan
+  bring = scan
+  bring.downcase!
 end
 
-stuff = food.each_line
-stuff.delete(bring)
+
+food.split(/\n/).delete(bring)
+new_food = food
+File.open('foods.csv', 'w') do |f|
+  f.puts(new_food)
+end
 
 print "Thanks! We'll see you there!"
